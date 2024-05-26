05709ec657d2f9c5d617e0c73d9081c959adaae9
bsdaoquang <bsdaoquang@gmail.com>
2024-05-26 22:09:41 +0700

    add delete api

--- server/index.js
+++ server/index.js
@@ -1,118 +1,58 @@
 import express from 'express'
-import {posts} from './data/posts.js'
+import {db} from './db.js';
+import cors from 'cors';
 
 const app = express()
 const PORT = 3001
 
 app.use(express.json())
-
-// CRUD
-
-app.get('/', (req, res) => {
-  res.send('Hello MindX')
-})
+app.use(cors())
 
 app.get('/posts', (req, res) => {
   res.status(200).json({
     message: 'Posts',
-    data: {items: posts, totalItem: posts.length}
+    data: db.posts
+  })
+})
+app.get('/users', (req, res) => {
+  res.status(200).json({
+    message: 'Users',
+    data: db.users
   })
 })
 
-app.get('/post/:id', (req, res) => {
+app.get('/user/:id', (req, res) => {
+
   const {id} = req.params
+  const item = db.users.find(element => `${element.id}` === id)
 
-  const item = posts.find(element => `${element.id}` === id)
-
-  if (item) {
-    res.status(200).json({
-      message: 'Post detail',
-      data: item
-    })
+  if (!item) {
+    throw  new Error('User not found')
   }
 
-  res.status(403).json({
-    message: 'Post not found',
-    data: []
+  res.status(200).json({
+    message: 'User',
+    data: item
   })
 })
 
-app.get('/detail', (req, res) => {
-  const {id} = req.query
-  const item = posts.find(element => `${element.id}` === id)
+app.delete('/post/:id', (req, res) => {
 
-  if (item) {
-    res.status(200).json({
-      message: 'Post detail',
-      data: item
-    })
-  }
-
-  res.status(403).json({
-    message: 'Post not found',
-    data: []
-  })
-})
-app.delete('/delete-post', (req, res) => {
-  const {id} = req.query
-
-  const index = posts.findIndex(element => `${element.id}` === id)
+  const {id} = req.params
+  const index = db.posts.findIndex(element => `${element.id}` === id)
 
   if (index !== -1) {
-    posts.splice(index, 1)
+    db.posts.splice(index, 1)
 
-    res.status(203).json({
-      message: 'Post is deleted',
-      data: {
-        totalItem: posts.length
-      }
+    res.status(200).json({
+      message: 'Deleted',
+      data: null
     })
   }else{
-    res.status(404).json({
-      message: 'Data not found',
-      data: []
-    })
+    throw new Error('Post not found!!!')
   }
-
 })
 
-app.post('/add-new-post', (req, res) => {
-  
-  const item = {
-    id: Math.floor(Math.random() * 1000000),
-    ...req.body
-  }
-  posts.push(item)
-
-  res.status(201).json({
-    message: 'Add new post successfully',
-    data: {item, totalItem: posts.length}
-  })
-})
-
-app.put('/update-post', (req, res) => {
-
-  const {id} = req.query
-  const body = req.body
-
-  const index = posts.findIndex(element=> `${element.id}` === id)
-
-  if (index !== -1) {
-    posts[index] = body
-
-    res.status(201).json({
-      message: 'Update post successfully',
-      data: posts[index]
-    })
-  }else{
-    res.status(404).json({
-      message: 'Post not found',
-      data: []
-    })
-  }
-
-  
-})
 
 app.listen(PORT, (err) => {
   if (err) {
